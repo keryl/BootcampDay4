33429cebfc4593af1f0bd6ce0c9d916b50934613
keryl <kerylklonye@gmail.com>
2017-03-09 14:42:15 +0300

    Implement find_missing func
    Return any number that has 1 occurrence

--- find_missing.py
+++ find_missing.py
@@ -5,8 +5,19 @@ def find_missing(list1, list2):
 
     numbers = {}
 
+    # go through both lists and add occurrences to numbers
+
+    for num in list1:
+        occurrence = numbers.get(num, 0) # get any existing occurrence of num
+        numbers[num] = occurrence + 1 # increment occurrence by 1 and save it back to the dict
+
+    for num in list2:
+        occurrence = numbers.get(num, 0) # get any existing occurrence of num
+        numbers[num] = occurrence + 1 # increment occurrence by 1 and save it back to the dict
+
     # loop through the dictionary and return if a a number that occurred once is found
 
     for number, occurrence in numbers.items():
         if occurrence == 1:
             return number
+
