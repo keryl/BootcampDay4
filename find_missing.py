d84428911e0730ad7753c6b910da566749c3abbc
keryl <kerylklonye@gmail.com>
2017-03-09 14:35:07 +0300

    Pseudocode for find_missing

--- find_missing.py
+++ find_missing.py
@@ -1,3 +1,12 @@
-def find_missing():
+def find_missing(list1, list2):
 
-    pass
+    # initialize dict to store occurrences of numbers in both lists e.g.
+    # numbers = { 1: 2, 3: 1 }
+
+    numbers = {}
+
+    # loop through the dictionary and return if a a number that occurred once is found
+
+    for number, occurrence in numbers.items():
+        if occurrence == 1:
+            return number
