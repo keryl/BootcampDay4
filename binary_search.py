510bee9b3f030d0168aeb5299ad8d0220dfa5a02
keryl <kerylklonye@gmail.com>
2017-03-09 15:10:57 +0300

    Binary Search should be populated with nums on init

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,11 @@
 class BinarySearch(list): # class inherits list
 
-    pass
+    def __init__(self, a, b):
+
+        self.length = a
+
+        for num in range(b, a * b + b, b):
+            self.append(num)
+
+if __name__ == "__main__":
+    print(BinarySearch(20, 2))
